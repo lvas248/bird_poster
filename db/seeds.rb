293eecc61bac801d02d2ba80dd721408593ad7d9
Luis <lvas248@gmail.com>
2022-12-09 16:28:32 -0500

    manual deploy test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,29 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-10.times do 
+
+
+arr = ['Blue Jay', 'Cardinal', 'Wood Pecker', 'Bald Eagle', 'Sparrow', 'Raven', 'Vulture', 'Owl', 'Crow', 'King
+fisher']
+
+image = [
+    'https://images.unsplash.com/photo-1623369026097-07d7aab15898?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Qmx1ZSUyMEpheXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1615146101981-cf25d1a1e6a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Q2FyZGluYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1648078505331-c910a8102e3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8V29vZCUyMFBlY2tlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1648078505331-c910a8102e3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8V29vZCUyMFBlY2tlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1539664030485-a936c7d29c6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3BhcnJvd3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1433888376991-1297486ba3f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8UmF2ZW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1558907530-a0827a86fb4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8VnVsdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1553264701-d138db4fd5d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8T3dsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1559649191-b9d89ad31819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Q3Jvd3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1486365227551-f3f90034a57c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8S2luZ0Zpc2hlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+]
+
+i = 0
+while i <= arr.length 
     Bird.create(
-        name: Faker::Creature::Bird.common_family_name
+        name: arr[i], 
+        image_url: image[i]
     )
+    i+=1
 end
